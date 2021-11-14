fc0ee053e30870acebd937e2cc23fa6c5d0273cb
Moon Hyo Jae <followmoonrise@gmail.com>
2021-11-14 22:44:28 +0900

    feat : CardOnFileDashboard, header 추가

--- MiniSuperApp/FinanceHome/CardOnFileDashboard/CardOnFileDashboardViewController.swift
+++ MiniSuperApp/FinanceHome/CardOnFileDashboard/CardOnFileDashboardViewController.swift
@@ -9,12 +9,70 @@ import ModernRIBs
 import UIKit
 
 protocol CardOnFileDashboardPresentableListener: AnyObject {
-    // TODO: Declare properties and methods that the view controller can invoke to perform
-    // business logic, such as signIn(). This protocol is implemented by the corresponding
-    // interactor class.
+  // TODO: Declare properties and methods that the view controller can invoke to perform
+  // business logic, such as signIn(). This protocol is implemented by the corresponding
+  // interactor class.
 }
 
 final class CardOnFileDashboardViewController: UIViewController, CardOnFileDashboardPresentable, CardOnFileDashboardViewControllable {
+  
+  weak var listener: CardOnFileDashboardPresentableListener?
+  
+  private let headerStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.alignment = .fill
+    stackView.distribution = .equalSpacing
+    stackView.axis = .horizontal
+    return stackView
+  }()
+  
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.font = .systemFont(ofSize: 22, weight: .semibold)
+    label.text = "카드 및 계좌"
+    return label
+  }()
+  
+  private lazy var seeAllButton: UIButton = {
+    let button = UIButton()
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.setTitle("전체보기", for: .normal)
+    button.setTitleColor(.systemBlue , for: .normal)
+    button.addTarget(self, action: #selector(seeAllButtonTapped), for: .touchUpInside)
+    return button
+  }()
+  
+  
+  init() {
+    super.init(nibName: nil, bundle: nil)
+    self.setUpView()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setUpView() {
+    view.addSubview(headerStackView)
+    
+    [titleLabel, seeAllButton].forEach {
+      headerStackView.addArrangedSubview($0)
+    }
+    
+    NSLayoutConstraint.activate([
+      headerStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+      headerStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      headerStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+    ])
+  }
+}
 
-    weak var listener: CardOnFileDashboardPresentableListener?
+// MARK: - Event Method
+extension CardOnFileDashboardViewController {
+  @objc
+  private func seeAllButtonTapped() {
+    
+  }
 }

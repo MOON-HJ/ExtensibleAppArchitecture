2a187fad394fe0cbbf319e016c7f7f87f5d1b2d2
Moon Hyo Jae <followmoonrise@gmail.com>
2022-02-06 23:11:10 +0900

    fix : TransportHome Riblet "ModernRIBs" 디펜던시 추가

--- Transport/Package.swift
+++ Transport/Package.swift
@@ -13,15 +13,16 @@ let package = Package(
             targets: ["TransportHome"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+      .package(name: "ModernRIBs", url: "https://github.com/DevYeom/ModernRIBs", .exact("1.0.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TransportHome",
-            dependencies: [],
+            dependencies: [
+              "ModernRIBs"
+            ],
             resources: [
               .process("Resources")
             ]

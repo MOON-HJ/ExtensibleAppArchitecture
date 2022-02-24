8b3cad88fef9096055ec54ea6013b87206533882
Moon Hyo Jae <followmoonrise@gmail.com>
2022-02-24 23:36:38 +0900

    test : Topup 유닛테스트 보일러플레이트 코드 추가

---
+++ Finance/Tests/TopupImplTests/Topup/TopupInteractorTests.swift
@@ -0,0 +1,29 @@
+//
+//  TopupInteractorTests.swift
+//  MiniSuperApp
+//
+//  Created by 문효재 on 2022/02/24.
+//
+
+@testable import TopupImpl
+import XCTest
+
+final class TopupInteractorTests: XCTestCase {
+  
+  private var interactor: TopupInteractor!
+  
+  // TODO: declare other objects and mocks you need as private vars
+  
+  override func setUp() {
+    super.setUp()
+    
+    // TODO: instantiate objects and mocks
+  }
+  
+  // MARK: - Tests
+  
+  func test_exampleObservable_callsRouterOrListener_exampleProtocol() {
+    // This is an example of an interactor test case.
+    // Test your interactor binds observables and sends messages to router or listener.
+  }
+}
---
+++ Finance/Tests/TopupImplTests/Topup/TopupRouterTests.swift
@@ -0,0 +1,29 @@
+//
+//  TopupRouterTests.swift
+//  MiniSuperApp
+//
+//  Created by 문효재 on 2022/02/24.
+//
+
+@testable import TopupImpl
+import XCTest
+
+final class TopupRouterTests: XCTestCase {
+  
+  private var router: TopupRouter!
+  
+  // TODO: declare other objects and mocks you need as private vars
+  
+  override func setUp() {
+    super.setUp()
+    
+    // TODO: instantiate objects and mocks
+  }
+  
+  // MARK: - Tests
+  
+  func test_routeToExample_invokesToExampleResult() {
+    // This is an example of a router test case.
+    // Test your router functions invokes the corresponding builder, attachesChild, presents VC, etc.
+  }
+}
